1379ff271a97d0186a7d3d8ca50928a0f4e23127
Takanori Sudo <sanboysoney3@gmail.com>
2017-11-30 13:34:15 +0900

    ここからスタート

--- index.php
+++ index.php
@@ -1,7 +1,25 @@
 <!--処理-->
 <?php
     session_start();
-
+    $status ="none";
+    if(isset($_SESSION["id"]))
+    {
+        header("Location: ./login/index.php");
+		exit();
+    } 
+    else
+    {
+        try
+        {
+            $db = new PDO('mysql:host=localhost;dbname=kelp_book;charset=utf8','kelp_book','cyber');
+            $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        }
+        catch(PDOException $e)
+        {
+            exit('データベース処理失敗:'.$e->getMessage());   
+        }
+        $status = "empty";
+    }
 ?>
 <!--html-->
 <!DOCTYPE html>
@@ -11,6 +29,10 @@
         <title></title>
     </head>
     <body>
-        
+        <?php if($status == "empty") :?>
+
+        <?php elseif($status == "  ") :?>
+
+        <?php endif;?>
     </body>
 </html>
