9728e21b66f40034a1181ae36271316bcc0a60a1
Takanori Sudo <sanboysoney3@gmail.com>
2017-11-30 13:21:49 +0900

    index.php 追加

---
+++ index.php
@@ -0,0 +1,16 @@
+<!--処理-->
+<?php
+    session_start();
+
+?>
+<!--html-->
+<!DOCTYPE html>
+<html lang ="ja">
+    <head>
+        <meta charset ="utf-8">
+        <title></title>
+    </head>
+    <body>
+        
+    </body>
+</html>
