7d4b9f8a0d3c41546b474eba96fc5c303ab5c9c9
Takanori Sudo <sanboysoney3@gmail.com>
2017-12-13 15:40:47 +0900

    ログインハッシュ化
    ちょっとログインできないから待ってて

--- index.php
+++ index.php
@@ -1,99 +1,103 @@
-<!--処理-->
 <?php
     session_start();
-    $status ="none";
-    $inputerror ="false";
-    $iderror ="false";
-    $passerror ="false";
 
-    if(isset($_SESSION["id"]))
-    {
-        header("Location: ./login/index.php");
-		exit();
-    } 
-    else
-    {
-        try
-        {
-            $db = new PDO('mysql:host=localhost;dbname=kelp_book;charset=utf8','kelp_book','cyber');
-            $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+	$status = "none";
+	$inputerror = "false";
+	$emptyerror = "false";
+	$fatalerror = "false";
+    
+    try{
+        $db = new PDO('mysql:host=localhost;dbname=project;charset=utf8','j15253','ikkaku3boy');
+        $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        if(isset($_SESSION["id"]))
+		{
+		    $status = "allready";
+		}
+		else if(!empty($_POST["id"]) && !empty($_POST["password"]))
+		{
+            $id        = $_POST['id'];
+            $password  = $_POST['password'];
+            
+            $sql = $db->prepare("SELECT password,name,manager,address FROM user WHERE id = ?");
+            $sql->bindValue(1,"{$id}");
+            $sql->execute();
 
-            if(!empty($_POST['id']) && !empty($_POST['password']))
+            if($sql->rowCount() == 1)
             {
-                $id =$_POST['id'];
-                $password =$_POST['password'];
-
-                $sql =$db->prepare("SELECT * FROM user WHERE id = ?");
-                $sql->bindValue(1,$id);
-                $sql->execute();
-                
-                $i =0;
-                while($row =$sql->fetch())
+                $row = $sql->fetch();
+                if(password_verify ($password, $row['password']))
                 {
-                    if($password == $row['password'])
-                    {
-                        $_SESSION['id'] =$id;
-                        $_SESSION['name'] =$row['name'];
-                        $_SESSION['manager'] =$row['manager'];
-                        $status ="success";
-                    }
-                    else
-                    {
-                        $status ="empty";
-                        $passerror ="true";
-                    }
-                    $i++;
+                    $_SESSION["id"]      = $id;
+                    $_SESSION["name"]    = $row['name'];
+                    $_SESSION["manager"] = $row['manager'];
+                    $status = "success";
                 }
-                if($i == 0)
+                else
                 {
-                    $status ="empty";
-                    $iderror ="true";
+					$status = "empty";
+					$inputerror = "true";
                 }
-            }
-            else if(!empty($_POST['id']) || !empty($_POST['password']))
-            {
-                $status ="empty";
-                $inputerror ="true";
             }
             else
             {
-                $status = "empty";
+				$status = "empty";
+				$fatalerror = "true";
             }
         }
-        catch(PDOException $e)
+        else
         {
-            exit('データベース処理失敗:'.$e->getMessage());   
+			$sql = $db->prepare("SELECT * FROM user");
+            $sql->execute();
+			if($sql->rowCount() == 0)
+			{
+				$_SESSION['first'] = "yes";
+				header("Location: ./first.php");
+				exit();
+			}
+			else
+			{
+				$status = "empty";
+				if(!empty($_POST['id']) || !empty($_POST['password']))
+				{
+					$emptyerror = "true";
+				}
+			}
         }
-        
+    }
+    catch(PDOException $e)
+    {
+        exit('データベース処理失敗:'.$e->getMessage());   
     }
 ?>
 
-<!--html-->
 <!DOCTYPE html>
-<html lang ="ja">
-    <head>
-        <meta charset ="utf-8">
-        <title></title>
-    </head>
-    <body>
-        <?php if($status == "empty") :?>
-            <h1>コンピュータ部備品管理</h1>
-            ログインしてください<br>
-            <form action ="index.php" method ="POST">
-                <table>
-                    <tr><td>ID</td><td><input type ="text" name ="id"></td></tr>
-                    <tr><td>PASSWORD</td><td><input type ="password" name ="password"></td></tr>
-                </table><br>
-                <input type ="submit" value ="ログイン">
-            </form>
-            <?php if($inputerror == "true") echo "未入力があります。";?>
-            <?php if($iderror == "true") echo "入力されたIDは存在しません。";?>
-            <?php if($passerror == "true") echo "パスワードが間違っています。";?>
-        <?php elseif($status == "success") :?>
-            <?php 
-                header("Location : book/top.php");
-                exit();
-            ?>
-        <?php endif;?>
-    </body>
+<html lang="ja">
+	<head>
+		<meta charset="UTF-8">
+		<title>kelp連絡網</title>
+	</head>
+	<body>
+		<h1>連絡網</h1>
+		<?php  if($status == "allready") :
+		    header("Location: user/index.php");
+			exit();
+		?>
+	    <?php elseif ($status == "success") :
+		    header("Location: user/index.php");
+			exit();
+		?>
+		<?php elseif($status == "empty") : ?>
+		    <form action='index.php' method='POST'>
+			    ログインID:<input type='text' name='id'><br>
+			    パスワード：<input type='password' name='password'><br>
+			    <p><input type ='submit' value='ログイン'></p>
+			</form>
+			<?php if($inputerror == "true") echo "パスワードかIDが間違っています。"; ?>
+			<?php if($emptyerror == "true") echo "IDかパスワードが入力されていません。"; ?>
+			<?php if($fatalerror == "true") echo "不正なIDです。再度繰り返して変わらなければ管理者に連絡してください。"; ?>
+			<br><a href='forgetpass.php'>パスワードを忘れたらこちら</a><br>
+		<?php else :  ?>
+			エラー　クッキーを削除してやり直してください。
+		<?php endif; ?>
+	</body>
 </html>
